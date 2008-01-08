0c43347aac64047b4652bcaae51f0f52e46462a6
Michael Meskes <meskes@debian.org>
2008-01-08 09:42:28 +0000

    * Added debconf support. * Tell the user about losing snapshots and ask via debconf whether to proceed, closes: #458842.  Clarified error message coming up if module is not loaded, closes: #459440.

--- debian/virtualbox-ose.files/VBox.sh
+++ debian/virtualbox-ose.files/VBox.sh
@@ -45,7 +45,8 @@ fi
 if [ ! -c /dev/vboxdrv ]; then
     cat << EOF
 WARNING: The character device /dev/vboxdrv does not exist.
-	 Please install the virtualbox-ose-modules package for your kernel.
+	 Please install the virtualbox-ose-modules package for your kernel and
+	 load the module named vboxdrv into your system.
 
 	 You will not be able to start VMs until this problem is fixed.
 EOF
