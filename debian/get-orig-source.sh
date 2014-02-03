a6ad9d438f24258931a0f7efab2fc8a1c852bb35
Felix Geyer <fgeyer@debian.org>
2014-02-03 17:58:37 +0100

    get-orig-source.sh: use correct tarball name and base dir.
    virtualbox-VERSION/ instead of virtuabox-VERSION.orig/
    virtualbox_VERSION.orig.tar.xz instead of virtualbox-VERSION.orig.tar.xz

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -9,14 +9,13 @@ REAL_TARBALL=`readlink -f ${ORIG_TARBALL}`
 
 WORKING_DIR=`dirname ${ORIG_TARBALL}`
 
-ORIG_TARBALL_DFSG=`echo ${ORIG_TARBALL} | sed -e "s/\(${UPSTREAM_VERSION}\)\(\.orig\)/\1-dfsg\2/g"`
+ORIG_TARBALL_DFSG=`echo ${ORIG_TARBALL} | sed -e "s/\(${UPSTREAM_VERSION}\)\(\.orig\)/\1-dfsg/g"`
 ORIG_TARBALL_DIR=`echo ${ORIG_TARBALL_DFSG} | sed -e "s/_\(${UPSTREAM_VERSION}\)/-\1/g" -e "s/\.tar\.bz2//g"`
 ORIG_TARBALL_DIR_STRIP=`basename ${ORIG_TARBALL_DIR}`
-DEST_TARBALL_DIR=${ORIG_TARBALL_DIR%.orig}
-DEST_TARBALL_DIR_STRIP=`basename ${DEST_TARBALL_DIR}`
+DEST_TARBALL_NAME=`echo ${ORIG_TARBALL_DIR} | sed -e "s#-\(${UPSTREAM_VERSION}\)#_\1#g"`.orig.tar.xz
 
 mkdir -p ${ORIG_TARBALL_DIR}
-tar --directory=${ORIG_TARBALL_DIR} --strip 1 -xjf ${REAL_TARBALL} || exit 1 
+tar --directory=${ORIG_TARBALL_DIR} --strip 1 -xjf ${REAL_TARBALL} || exit 1
 rm -f  ${ORIG_TARBALL} ${REAL_TARBALL}
 
 rm -fr ${ORIG_TARBALL_DIR}/debian
@@ -39,7 +38,7 @@ find   ${ORIG_TARBALL_DIR}/src/VBox/Additions/x11/x11include -mindepth 1 -maxdep
    -type d ! -name '*mesa-*' -exec rm -rf {} \;
 
 
-tar --exclude .svn --exclude '.git*' --exclude debian --directory ${WORKING_DIR} -cJf ${DEST_TARBALL_DIR}.tar.xz ${ORIG_TARBALL_DIR_STRIP} || exit 1
+tar --exclude .svn --exclude '.git*' --exclude debian --directory ${WORKING_DIR} -cJf ${DEST_TARBALL_NAME} ${ORIG_TARBALL_DIR_STRIP} || exit 1
 rm -rf ${ORIG_TARBALL_DIR}
-echo "Done, now you can run git-import-orig --pristine-tar ${DEST_TARBALL_DIR}.tar.xz"
+echo "Done, now you can run git-import-orig ${DEST_TARBALL_NAME}.tar.xz"
 exit 0
