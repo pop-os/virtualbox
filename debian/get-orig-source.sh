18f35b6129dae8a7a41f847403febc3090e63f5f
Gianfranco Costamagna <costamagnagianfranco@yahoo.it>
2023-04-19 09:57:38 +0200

    Drop lzma too

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -45,6 +45,7 @@ rm -fr ${ORIG_TARBALL_DIR}/src/libs/openssl*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libxml2*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libxslt*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/liblzf*
+rm -fr ${ORIG_TARBALL_DIR}/src/libs/liblzma*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libogg*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libtpms*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libvorbis*
