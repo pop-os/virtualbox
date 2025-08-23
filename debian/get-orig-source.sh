2700a98e22273b5205a744227fb443b1d16d785b
Gianfranco Costamagna <locutusofborg@debian.org>
2025-08-23 15:30:16 +0200

    Also drop libjpeg-turbo

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -41,6 +41,7 @@ rm -f  ${ORIG_TARBALL_DIR}/src/libs/xpcom18a4/xpcom/MoreFiles/FSCopyObject.c
 rm -f  ${ORIG_TARBALL_DIR}/src/libs/xpcom18a4/xpcom/MoreFiles/FSCopyObject.h
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/curl*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libpng*
+rm -fr ${ORIG_TARBALL_DIR}/src/libs/libjpeg-turbo-*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/openssl*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libxml2*
 rm -fr ${ORIG_TARBALL_DIR}/src/libs/libxslt*
